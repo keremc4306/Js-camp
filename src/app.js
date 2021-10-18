94bdd54557d86b35ce3ed9cde00be74b2075e5a7
Kerem Candangil <keremc4306@gmail.com>
2021-10-18 23:19:12 +0300

    'Firstlecture'

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Merhaba Kodlama.io")
+
+//Define variable
+//JS is not type-safe
+let dolarT = 9.30
+
+let dolarYes = 9.20
+dolarYes = "Ankara"
+{
+    let dolarYes = 9.10
+}
+
+console.log(dolarYes)
+
+const euroYes = 11.2
+euroYes = 11
+
+console.log(euroYes)
+
+
+
